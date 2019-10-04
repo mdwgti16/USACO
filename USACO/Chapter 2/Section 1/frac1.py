187b61a58b571761901e48f27e7518767b43efff
mdwgti16 <47391432+mdwgti16@users.noreply.github.com>
2019-10-04 14:17:29 +0900

    frac-sol

--- USACO/Chapter 2/Section 1/frac1.py
+++ USACO/Chapter 2/Section 1/frac1.py
@@ -1,3 +1,4 @@
+# ANALYSIS
 """
 ID: xggc1
 LANG: PYTHON3
@@ -9,26 +10,24 @@ def getKey(s):
 
 fin = open('frac1.in','r')
 N = int(fin.readline().strip())
-# rooms=[list(map(int,line.strip().split(' '))) for line in fin.readlines()]
 def getGcd(a,b):
-#     print("GETGCD",a,b)
     q,r=divmod(a,b)
-#     print("Q=",q,"R=",r,"B",b)
     if r == 0:
-        return b
+        if b==1:
+            return 1
+        else:
+            return 0
     else :
         return getGcd(b,r)
 fracList=[]
 fracList.append("0/1")
 for den in range(1,N+1):
-    for num in range(1,N+1):
-        if den>=num:
-            gcd=getGcd(den,num)
-#             print(num,den,gcd)
-            frac=f"{int(num/gcd)}/{int(den/gcd)}"
-#                 print("A",frac)
+    for num in range(1,den+1):
+        if getGcd(den,num):
+            frac=f"{num}/{den}"
             if not frac in fracList:
                 fracList.append(frac)
+                
 fracList.sort(key=getKey)
 
 with open('frac1.out','w') as fout:
